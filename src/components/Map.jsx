4e834dd6b8c0e827c9ca4d7148efe35383656f8c
Josh Bryson <josh@bryson.me.uk>
2022-04-19 12:10:25 +0100

    Further file tidying

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,64 +1,19 @@
 import * as React from 'react';
 import { Controls } from './Controls';
 import { Canvas } from '@react-three/fiber';
-import { BoxGeometry } from 'three';
 import { MapContainer, MapFloor } from './MapCore';
 import { Clouds } from './Clouds';
 import { SeaMesh } from './SeaMesh';
 import { Hex } from './Hex';
 import SimplexNoise from 'simplex-noise';
-import { EnvMap, hexGeometry, tileToPosition } from './MapHelpers';
-import { mergeBufferGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
-
-import { tree } from './Tree';
-import { stone } from './Stone';
+import { dirtGeo, dirt2Geo, EnvMap, grassGeo, hex, sandGeo, stoneGeo, tileToPosition } from './MapHelpers';
 import Dirt from "../assets/dirt.png";
 import Dirt2 from "../assets/dirt2.jpg";
 import Grass from '../assets/grass.jpg';
 import Sand from '../assets/sand.jpg';
 import Stone from '../assets/stone.png';
 
-const STONE_HEIGHT = 10 * 0.8;
-const DIRT_HEIGHT = 10 * 0.7;
-const GRASS_HEIGHT = 10 * 0.5;
-const SAND_HEIGHT = 10 * 0.3;
-const DIRT2_HEIGHT = 10 * 0;
-
-const simplex = new SimplexNoise()
-
-let stoneGeo = new BoxGeometry(0,0,0);
-let dirtGeo = new BoxGeometry(0,0,0);
-let dirt2Geo = new BoxGeometry(0,0,0);
-let sandGeo = new BoxGeometry(0,0,0);
-let grassGeo = new BoxGeometry(0,0,0);
-
-function hex(height, position) {
-    let geo = hexGeometry(height, position);
-  
-    if(height > STONE_HEIGHT) {
-      stoneGeo = mergeBufferGeometries([geo, stoneGeo]);
-  
-      if(Math.random() > 0.8) {
-        stoneGeo = mergeBufferGeometries([stoneGeo, stone(height, position)]);
-      }
-    } else if(height > DIRT_HEIGHT) {
-      dirtGeo = mergeBufferGeometries([geo, dirtGeo]);
-  
-      if(Math.random() > 0.8) {
-        grassGeo = mergeBufferGeometries([grassGeo, tree(height, position)]);
-      }
-    } else if(height > GRASS_HEIGHT) {
-      grassGeo = mergeBufferGeometries([geo, grassGeo]);
-    } else if(height > SAND_HEIGHT) { 
-      sandGeo = mergeBufferGeometries([geo, sandGeo]);
-  
-      if(Math.random() > 0.8 && stoneGeo) {
-        stoneGeo = mergeBufferGeometries([stoneGeo, stone(height, position)]);
-      }
-    } else if(height > DIRT2_HEIGHT) {
-      dirt2Geo = mergeBufferGeometries([geo, dirt2Geo]);
-    } 
-}
+const simplex = new SimplexNoise();
 
 const Map = ({}, ref) => {
     for(let i = -20; i <= 20; i++) {
@@ -74,20 +29,41 @@ const Map = ({}, ref) => {
         }
     }
 
+    let geoOptions = [
+        {
+            geoType: stoneGeo,
+            colour: Stone
+        },
+            {
+            geoType: dirtGeo,
+            colour: Dirt
+        },
+        {
+            geoType: dirt2Geo,
+            colour: Dirt2
+        },
+        {
+            geoType: sandGeo,
+            colour: Sand
+        },
+        {
+            geoType: grassGeo,
+            colour: Grass
+        }
+    ];
+
     return (
         <Canvas camera={{ fov: 75, near: 10.9, far: 1000, position: [-17, 23, 10] }}>
             <Controls />
-                <color attach="background" args={["#FFEECC"]} />
-                <directionalLight color="#FFEECC" position={[10, 20, 10]} />
-                <Clouds />
-                <Hex geo={stoneGeo} texture={Stone} />
-                <Hex geo={grassGeo} texture={Grass} />
-                <Hex geo={dirt2Geo} texture={Dirt2} />
-                <Hex geo={dirtGeo} texture={Dirt} />
-                <Hex geo={sandGeo} texture={Sand} />
-                <SeaMesh />
-                <MapFloor />
-                <MapContainer />
+            <color attach="background" args={["#FFEECC"]} />
+            <directionalLight color="#FFEECC" position={[10, 20, 10]} />
+            <Clouds />
+            {geoOptions.map(({geoType, colour}) => (
+                <Hex geo={geoType} texture={colour} />
+            ))}
+            <SeaMesh />
+            <MapFloor />
+            <MapContainer />
         </Canvas>
     )
 }
--- src/components/MapHelpers.js
+++ src/components/MapHelpers.js
@@ -1,8 +1,17 @@
 import { useLoader, useThree } from '@react-three/fiber';
-import { CylinderBufferGeometry, Vector2 } from 'three';
+import { BoxGeometry, CylinderBufferGeometry, Vector2 } from 'three';
 import { PMREMGenerator } from 'three/src/extras/PMREMGenerator';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { mergeBufferGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 import HDR from '../assets/envmap.hdr';
+import { tree } from './Tree';
+import { stone } from './Stone';
+
+const STONE_HEIGHT = 10 * 0.8;
+const DIRT_HEIGHT = 10 * 0.7;
+const GRASS_HEIGHT = 10 * 0.5;
+const SAND_HEIGHT = 10 * 0.3;
+const DIRT2_HEIGHT = 10 * 0;
 
 const EnvMap = () => {
     const { gl } = useThree();
@@ -30,8 +39,39 @@ function tileToPosition(tileX, tileY) {
     return new Vector2((tileX + (tileY % 2) * 0.5) * 1.77, tileY * 1.535);
 }
 
+let stoneGeo = new BoxGeometry(0,0,0);
+let dirtGeo = new BoxGeometry(0,0,0);
+let dirt2Geo = new BoxGeometry(0,0,0);
+let sandGeo = new BoxGeometry(0,0,0);
+let grassGeo = new BoxGeometry(0,0,0);
+
+function hex(height, position) {
+    let geo = hexGeometry(height, position);
+
+    if(height > STONE_HEIGHT) {
+        stoneGeo = mergeBufferGeometries([geo, stoneGeo]);
+        Math.random() > 0.8 && (stoneGeo = mergeBufferGeometries([stoneGeo, stone(height, position)]))
+    } else if(height > DIRT_HEIGHT) {
+        dirtGeo = mergeBufferGeometries([geo, dirtGeo]);
+        Math.random() > 0.8 && (grassGeo = mergeBufferGeometries([grassGeo, tree(height, position)]))
+    } else if(height > GRASS_HEIGHT) {
+        grassGeo = mergeBufferGeometries([geo, grassGeo]);
+    } else if(height > SAND_HEIGHT) { 
+        sandGeo = mergeBufferGeometries([geo, sandGeo]);
+        Math.random() > 0.8 && (stoneGeo = mergeBufferGeometries([stoneGeo, stone(height, position)]))
+    } else if(height > DIRT2_HEIGHT) {
+        dirt2Geo = mergeBufferGeometries([geo, dirt2Geo]);
+    }
+}
+
 export {
+    dirtGeo,
+    dirt2Geo,
     EnvMap,
+    grassGeo,
+    hex,
     hexGeometry,
+    sandGeo,
+    stoneGeo,
     tileToPosition
 }
